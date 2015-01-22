89e897896aff0f0e2044f9ccd5a1b04a4004894d
James White <jameswhite@potatolondon.com>
2015-01-22 13:07:57 +0000

    Allow customization of TREAT_CONTENT_AS_HTML_TAGS and SELF_CLOSING_HTML_TAGS constants via the Django settings module

--- contentious/constants.py
+++ contentious/constants.py
@@ -1,4 +1,10 @@
-SELF_CLOSING_HTML_TAGS = ['img', 'br', 'hr', 'meta']
+from django.conf import settings
+
+
+SELF_CLOSING_HTML_TAGS = getattr(settings,
+    'CONTENTIOUS_SELF_CLOSING_HTML_TAGS', ['img', 'br', 'hr', 'meta'])
+
 
 #Note, the Javascript plugin has its own seprate copy of this:
-TREAT_CONTENT_AS_HTML_TAGS = ['div', 'select', 'ul']
+TREAT_CONTENT_AS_HTML_TAGS = getattr(settings,
+    'CONTENTIOUS_TREAT_CONTENT_AS_HTML_TAGS', ['div', 'select', 'ul'])
